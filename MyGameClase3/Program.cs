e7bc8746d84da4fac27f7a34a4d0fdab8eaf6e66
Alex <aguione@gmail.com>
2019-11-26 21:59:34 -0500

    ejercicios math

--- MyGameClase3/Program.cs
+++ MyGameClase3/Program.cs
@@ -8,6 +8,28 @@ namespace MyGameClase3 {
     class Program {
         static void Main (string[] args)
         {
+            int x = 5;
+            float y = 4.5f;
+            string str = "la old confiable";
+            bool myBool = false;
+            float result = 5 + 7;
+
+            Console.WriteLine (result + " valor: " + myBool);
+
+            result = 5 - 7;
+            Console.WriteLine (result);
+
+            result = 5 * 7;
+            Console.WriteLine (result);
+
+            result = 5f / 7f;
+            Console.WriteLine (result);
+
+            result = 5 % 2;
+            Console.WriteLine ("modulo: " + result);
+
+            Console.WriteLine ("la " + Console.ReadLine () + " confiable" );
+            Console.ReadLine ();
         }
     }
 }
